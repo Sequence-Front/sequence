a2e965d5709a5c26b247f538ae45fa49176dc663
seungyun-Park <tmdrbs0925@gmail.com>
2024-11-04 21:25:43 +0900

    gh-pages-setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import MainPage from './main/MainPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <MainPage/>
-    </>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
