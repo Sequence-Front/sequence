3ec2534c476d54d0537234335574462465d368b0
seungyun-Park <tmdrbs0925@gmail.com>
2024-11-17 20:55:52 +0900

    mypage_history_portfolio 1차 구현

--- src/mypage/MyPage.tsx
+++ src/mypage/MyPage.tsx
@@ -1,10 +1,35 @@
+// 2024-11-17 승균 작성
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../asset/component/Header';
+import Profile from './component/ProfileHeader';
+import PersonalHistory from './component/PersonalHistory';
+import Portfolio from './component/Portfolio';
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #151515;
+  color: #fff;
+  padding-top: 120px;
+  
+  @media (max-width: 768px) {
+    padding-top: 100px;
+  }
+`;
 
 const MyPage = () => {
     return (
-        <div>
-            Mypage
-        </div>
+        <>
+            <Header headerName="MyPage" />
+            <Container>
+                <Profile />
+                <PersonalHistory />
+                <Portfolio />
+            </Container>
+        </>
     );
 };
 
