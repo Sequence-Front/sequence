d3c42db8f690efbbbfe1d5406f4681d2ce10663d
Seunggyun park <tmdrbs0925@gmail.com>
2025-04-11 17:19:30 +0900

    [FEAT]팀원평가API연동완료

--- src/mypage/MyPage.tsx
+++ src/mypage/MyPage.tsx
@@ -1,15 +1,15 @@
 // 2024-11-18 18:09 승균 작성
 // 2024-11-19 18:47 준용 작성
-import React, {useState, useEffect} from 'react';
-import { useLocation } from 'react-router-dom';
-import styled from 'styled-components';
-import Profile from './component/ProfileHeader';
-import PersonalHistory from './page/PersonalHistory';
-import Portfolio from './page/Portfolio';
-import MemberEvaluation from './page/MemberEvaluation';
-import MyActivity from './page/MyActivity';
-import { getMyInfo } from '../api/myInfo';
-import { HistoryDataType } from './types/history.types';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import Profile from "./component/ProfileHeader";
+import PersonalHistory from "./page/PersonalHistory";
+import Portfolio from "./page/Portfolio";
+import MemberEvaluation from "./page/MemberEvaluation";
+import MyActivity from "./page/MyActivity";
+import { getMyInfo } from "../api/myInfo";
+import { HistoryDataType } from "./types/history.types";
 
 const Container = styled.div`
   width: 100%;
@@ -20,15 +20,14 @@ const Container = styled.div`
   background-color: #151515;
   color: #fff;
   padding-top: 120px;
-  
-`
+`;
 
 const TabContainer = styled.div`
   display: flex;
   justify-content: space-between;
   width: 75%;
   margin-bottom: clamp(3rem, 5vw, 5rem);
-`
+`;
 
 interface TabButtonProps {
   isActive: boolean;
@@ -39,23 +38,23 @@ const TabButton = styled.button<TabButtonProps>`
   padding: clamp(0.3rem, 2vw, 1.5rem);
   margin-top: clamp(0.5rem, 2vw, 2rem);
   border: none;
-  background-color: ${({ isActive }) => (isActive ? '#E32929' : '#151515')};
-  color: #F5F5F5;
+  background-color: ${({ isActive }) => (isActive ? "#E32929" : "#151515")};
+  color: #f5f5f5;
   cursor: pointer;
-  border: 1px solid ${({isActive}) => (isActive? '#E32929': '#9E9E9E')};
+  border: 1px solid ${({ isActive }) => (isActive ? "#E32929" : "#9E9E9E")};
   font-size: clamp(0.6rem, 2vw, 1.3rem);
-  font-family: 'Noto Sans';
+  font-family: "Noto Sans";
 
   &:hover {
-    background-color: ${({ isActive }) => (isActive ? '#E32929' : '#151515')};
+    background-color: ${({ isActive }) => (isActive ? "#E32929" : "#151515")};
   }
-`
+`;
 
-type Tab = 'PersonalHistory' | 'Portfolio' | 'MemberEvaluation' | 'MyActivity';
+type Tab = "PersonalHistory" | "Portfolio" | "MemberEvaluation" | "MyActivity";
 
 const ContentContainer = styled.div<{ isPortfolio: boolean }>`
-  width: ${({ isPortfolio }) => (isPortfolio ? '90%' : '75%')};
-`
+  width: ${({ isPortfolio }) => (isPortfolio ? "90%" : "75%")};
+`;
 
 interface MyPageData {
   basicInfo: {
@@ -100,15 +99,15 @@ interface MyPageData {
   teamFeedback: {
     myPageEvaluation: {
       keywords: Array<{
-          content: string;
-          count: number;
-        }>;
+        content: string;
+        count: number;
+      }>;
       feedbacks: Array<{
         content: string;
         duration: string;
-      }>
-    }
-  }
+      }>;
+    };
+  };
   portfolio: {
     archivePage: {
       content: Array<{
@@ -125,18 +124,18 @@ interface MyPageData {
       title: string;
       inviteDate: string;
       commentCount: number;
-    }>
+    }>;
   };
 }
 
 const MyPage = () => {
   const location = useLocation();
-  const [activeTab, setActiveTab] = useState<Tab>('PersonalHistory');
+  const [activeTab, setActiveTab] = useState<Tab>("PersonalHistory");
   const [profileData, setProfileData] = useState<MyPageData | null>(null);
-  const nickname = localStorage.getItem('nickname');
+  const nickname = localStorage.getItem("nickname");
 
   const params = new URLSearchParams(location.search);
-  const queryNickname = params.get('nickname');
+  const queryNickname = params.get("nickname");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -146,12 +145,12 @@ const MyPage = () => {
         const response = await getMyInfo();
         setProfileData(response);
       } catch (error) {
-        console.error('프로필 데이터 로딩 실패:', error);
+        console.error("프로필 데이터 로딩 실패:", error);
       } finally {
         setLoading(false);
       }
     };
-    
+
     fetchData();
   }, [queryNickname]);
 
@@ -161,13 +160,13 @@ const MyPage = () => {
     if (!profileData) return null;
 
     switch (activeTab) {
-      case 'PersonalHistory':
+      case "PersonalHistory":
         return <PersonalHistory careerHistory={profileData.careerHistory} />;
-      case 'Portfolio':
+      case "Portfolio":
         return <Portfolio portfolioData={profileData.portfolio} />;
-      case 'MemberEvaluation':
-        return <MemberEvaluation />;
-      case 'MyActivity':
+      case "MemberEvaluation":
+        return <MemberEvaluation evaluationData={ profileData.teamFeedback }/>;
+      case "MyActivity":
         return <MyActivity activities={profileData.myActivities} />;
       default:
         return <PersonalHistory careerHistory={profileData.careerHistory} />;
@@ -177,52 +176,55 @@ const MyPage = () => {
   return (
     <>
       {loading || !profileData ? (
-      <div style={{ color: 'white', paddingTop: '200px', textAlign: 'center' }}>
-        불러오는 중...
-      </div>
-    ) : (
-      <Container>
-        <Profile 
-          name={profileData?.basicInfo.nickname} 
-          birth={profileData?.basicInfo.birth}
-          skills={profileData?.basicInfo.skillCategory || []}
-          desiredJobs={profileData?.basicInfo.desiredJob || []}
-          // introduction={profileData?.careerHistory.introduction || ""}
-        />
-        <TabContainer>
-          <TabButton 
-            isActive={activeTab === 'PersonalHistory'} 
-            onClick={() => setActiveTab('PersonalHistory')}
-          >
-            경력 및 활동이력
-          </TabButton>
-          <TabButton 
-            isActive={activeTab === 'Portfolio'} 
-            onClick={() => setActiveTab('Portfolio')}
-          >
-            포트폴리오
-          </TabButton>
-          {/* <TabButton 
-            isActive={activeTab === 'MemberEvaluation'} 
-            onClick={() => setActiveTab('MemberEvaluation')}
-          >
-            팀원 평가
-          </TabButton> */}
-         
-          {isOwnProfile && (
-            <TabButton 
-              isActive={activeTab === 'MyActivity'} 
-              onClick={() => setActiveTab('MyActivity')}
+        <div
+          style={{ color: "white", paddingTop: "200px", textAlign: "center" }}
+        >
+          불러오는 중...
+        </div>
+      ) : (
+        <Container>
+          <Profile
+            name={profileData?.basicInfo.nickname}
+            birth={profileData?.basicInfo.birth}
+            skills={profileData?.basicInfo.skillCategory || []}
+            desiredJobs={profileData?.basicInfo.desiredJob || []}
+            // introduction={profileData?.careerHistory.introduction || ""}
+          />
+          <TabContainer>
+            <TabButton
+              isActive={activeTab === "PersonalHistory"}
+              onClick={() => setActiveTab("PersonalHistory")}
             >
-              내 활동
+              경력 및 활동이력
             </TabButton>
-          )}
-        </TabContainer>
-        <ContentContainer isPortfolio={activeTab === 'Portfolio'}>
-          {renderContent()}
-        </ContentContainer>
-      </Container>
-    )}
+            <TabButton
+              isActive={activeTab === "Portfolio"}
+              onClick={() => setActiveTab("Portfolio")}
+            >
+              포트폴리오
+            </TabButton>
+
+            <TabButton
+              isActive={activeTab === "MemberEvaluation"}
+              onClick={() => setActiveTab("MemberEvaluation")}
+            >
+              팀원 평가
+            </TabButton>
+
+            {isOwnProfile && (
+              <TabButton
+                isActive={activeTab === "MyActivity"}
+                onClick={() => setActiveTab("MyActivity")}
+              >
+                내 활동
+              </TabButton>
+            )}
+          </TabContainer>
+          <ContentContainer isPortfolio={activeTab === "Portfolio"}>
+            {renderContent()}
+          </ContentContainer>
+        </Container>
+      )}
     </>
   );
 };
--- src/mypage/component/MemberSummaryComponent.tsx
+++ src/mypage/component/MemberSummaryComponent.tsx
@@ -1,26 +1,27 @@
 // 11-19 1909 정준용
 // 멤버 평가 도표 컴포넌트
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 export type DataItem = {
-  label: string;
-  value: number;
-  profile: string;
   content: string;
-}
+  count: number;
+  profile?: string; // Optional for backward compatibility
+  label?: string; // Optional for backward compatibility
+  value?: number; // Optional for backward compatibility
+};
 
 type EvaluationBarProps = {
   item: DataItem;
   maxValue: number;
-}
+};
 
 const BarContainer = styled.div`
   display: flex;
   width: 100%;
   align-items: center;
   background-color: none;
-`
+`;
 
 const BarWrapper = styled.div`
   display: flex;
@@ -30,28 +31,29 @@ const BarWrapper = styled.div`
   justify-content: space-between;
   overflow: hidden;
   margin-bottom: clamp(0.5rem, 1.5vw, 0.8rem);
-`
+`;
 
 const BarFill = styled.div<{ width: number }>`
   display: flex;
-  width: ${(props) => props.width}%; 
+  width: ${(props) => props.width}%;
   background-color: #616161;
   align-items: center;
   padding: clamp(0.2rem, 0.8vw, 0.3rem);
   color: #ffffff;
-  font-size: clamp(0.8rem, 1.5vw, 1rem); 
+  font-size: clamp(0.8rem, 1.5vw, 1rem);
   font-weight: bold;
   white-space: nowrap;
-`
+`;
 
-const ProfileImage = styled.img`
+const ProfileImage = styled.div`
   display: flex;
   width: clamp(1.5rem, 3vw, 2rem);
   height: clamp(1.5rem, 3vw, 2rem);
   border-radius: 50%;
-  margin-left: clamp(0.5rem, 1.5vw, 1rem); 
-  margin-right: clamp(1rem, 3vw, 2rem); 
-`
+  margin-left: clamp(0.5rem, 1.5vw, 1rem);
+  margin-right: clamp(1rem, 3vw, 2rem);
+  background-color: aliceblue;
+`;
 
 const BarValue = styled.div`
   display: flex;
@@ -59,27 +61,32 @@ const BarValue = styled.div`
   font-size: clamp(1rem, 2vw, 1.4rem);
   font-weight: bold;
   margin-right: clamp(0.5rem, 2vw, 1rem);
-`
+`;
 
 export const GrayBar = styled.div`
   width: 100%;
   height: clamp(1rem, 2vw, 1.5rem);
   background: linear-gradient(to bottom, #212121, #21212100);
   margin: clamp(0.3rem, 1vw, 0.5rem) auto;
-`
+`;
 
+export const MemberSummaryComponent = ({
+  item,
+  maxValue,
+}: EvaluationBarProps) => {
+  // Use the appropriate properties based on the interface
+  const displayValue = item.value !== undefined ? item.value : item.count;
+  const displayLabel = item.label !== undefined ? item.label : item.content;
 
-
-export const MemberSummaryComponent = ({item, maxValue}: EvaluationBarProps) => {
   return (
     <BarContainer>
-    <BarWrapper>
-      <BarFill width={(item.value / maxValue) * 100}>
-        <ProfileImage src={item.profile} alt="프로필 이미지" />
-        {item.label}
-      </BarFill>
-      <BarValue>{item.value}</BarValue>
-    </BarWrapper>
-  </BarContainer>
+      <BarWrapper>
+        <BarFill width={(displayValue / maxValue) * 100}>
+          <ProfileImage />
+          {displayLabel}
+        </BarFill>
+        <BarValue>{displayValue}</BarValue>
+      </BarWrapper>
+    </BarContainer>
   );
 };
